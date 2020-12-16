87b1e50d69728cf1b9d2beeb1b08a6b3bae6facc
jgh109702 <42138328+jgh109702@users.noreply.github.com>
2020-12-16 12:41:47 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,245 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (50 >= Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength))) {
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            . # # # .
             # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
             `)
+        OLED.writeStringNewLine("good")
     } else if (60 > Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength))) {
         basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
             . . . . .
-            . . . . .
-            . . . . .
-            # # # # #
-            # # # # #
+            . . # . .
             `)
+        OLED.writeStringNewLine("caution")
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.setVolume(97)
     } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        OLED.writeStringNewLine("warning")
+        music.setVolume(255)
+        music.playTone(988, music.beat(BeatFraction.Breve))
+    }
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+let caller = 0
+radio.setGroup(1)
+basic.forever(function () {
+    if (caller == 1) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 2) {
+        basic.showLeds(`
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 4) {
+        basic.showLeds(`
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 6) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 7) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 8) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 9) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 11) {
         basic.showLeds(`
             . . . . .
             . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 12) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 13) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 14) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 15) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (caller == 16) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            `)
+    } else if (caller == 17) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (caller == 18) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (caller == 19) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (caller == 20) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            `)
+    } else if (caller == 21) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (caller == 22) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            `)
+    } else if (caller == 23) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (caller == 24) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            `)
+    } else if (caller == 25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
             `)
     }
 })
-radio.setGroup(5)
 basic.forever(function () {
-    radio.sendNumber(0)
+    radio.sendNumber(1)
 })
